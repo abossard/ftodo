18d4acdc34567158cccfb0cd9c1406830c27ed00
unknown <abossard@gmail.com>
2021-03-06 10:37:37 +0100

    init

---
+++ ServiceApp/src/ServiceApp.Calendar/Library.fs
@@ -0,0 +1,43 @@
+﻿namespace ServiceApp.Calendar.Library
+
+open System
+
+type Calendar =
+    {
+        title: string
+        timezone: TimeZoneInfo
+        entries: CalendarEntry list
+    } 
+
+and CalendarEntry =
+    {
+        startTime: DateTime
+        duration: TimeSpan
+        title: string
+        calendar: Calendar
+    }
+
+type Month =
+    January = 1
+    | February = 2
+    | March = 3
+    | April = 4
+    | May = 5
+    | June = 6
+    | July = 7
+    | August = 8
+    | September = 9
+    | October = 10
+    | November = 11
+    | December = 12
+
+type DayCalendar =
+    {
+        entries: CalendarEntry list
+    }
+    
+type MonthCalendar =
+    {
+        month: Month
+        days: DayCalendar array
+    }
---
+++ ServiceApp/src/ServiceApp.DB/Main.fs
@@ -0,0 +1,30 @@
+﻿module ServiceApp.DB.Main
+
+open System
+open LiteDB
+open LiteDB.FSharp
+open LiteDB.FSharp.Extensions
+
+let mapper = FSharpBsonMapper()
+use db = new LiteDatabase("simple.db", mapper)
+
+type Genre = Rock | Pop | Metal
+
+type Album = {
+    Id: int
+    Name: string
+    DateReleased: DateTime
+    Genre: Genre
+}
+
+let albums = db.GetCollection<Album>("albums")
+
+let metallica = 
+    { Id = 1; 
+      Name = "Metallica";
+      Genre = Metal;
+      DateReleased = DateTime(1991, 8, 12) }
+
+albums.Insert(metallica)
+
+let result = albums.findOne <@ fun album -> album.Id = 1 @> 
